97f01d3929540b8bd1fa9166ab27a39213d18634
Rikoring 🌿 <74352630+Rikorik0@users.noreply.github.com>
2025-07-14 20:42:55 +0900

    Fixed
    but settings are not there anymore

--- index.tsx
+++ index.tsx
@@ -1,8 +1,5 @@
-import { Settings } from "@api/Settings";
-import { addPreSendListener, removePreSendListener } from "@api/MessageEvents";
-import definePlugin, { OptionType } from "@utils/types";
-
-var chaePrefix = Settings.plugins.Nya.chaePrefixValue;
+import { addMessagePreSendListener, removeMessagePreSendListener } from "@api/MessageEvents";
+import definePlugin from "@utils/types";
 
 var nyaWords = {
     '나': "냐",
@@ -33,63 +30,65 @@ var nyaWords = {
     '낱': "냩",
     '낲': "냪",
     '낳': "냫",
-}
-
-var nyaWords2 = {
-    '내': () => chaePrefix,
-    '넹': () => chaePrefix,
-    '넴': () => chaePrefix,
-    '넵': () => chaePrefix,
-    '냐': () => chaePrefix,
-    '님': () => chaePrefix,
-    '니': () => chaePrefix,
-    '다': () => "다" + chaePrefix,
-    '까': () => "까" + chaePrefix,
-    '네': () => "네" + chaePrefix,
-    '야': () => "야" + chaePrefix,
-    '꺼': () => "꺼" + chaePrefix,
-    '래': () => "래" + chaePrefix,
-    '해': () => "해" + chaePrefix,
-    '지': () => "지" + chaePrefix,
-    '라': () => "라" + chaePrefix,
-    '요': () => "요" + chaePrefix,
-    '가': () => "가" + chaePrefix,
-    '데': () => "데" + chaePrefix,
-    '돼': () => "돼" + chaePrefix,
-    '줘': () => "줘" + chaePrefix,
-    '마': () => "마" + chaePrefix,
-    '와': () => "와" + chaePrefix,
-    '어': () => "어" + chaePrefix,
-    '자': () => "자" + chaePrefix,
-    '죠': () => "죠" + chaePrefix,
-    '서': () => "서" + chaePrefix,
-    '게': () => "게" + chaePrefix,
-    '듯': () => "듯" + chaePrefix,
 };
 
-function refreshPrefix() { // someone help me fix these spaghetti
-    chaePrefix = Settings.plugins.Nya.chaePrefixValue
-}
+var nyaWords2 = {
+    '내': "냥",
+    '넹': "냥",
+    '넴': "냥",
+    '넵': "냥",
+    '냐': "냥",
+    '님': "냥",
+    '니': "냥",
+    '다': "다냥",
+    '까': "까냥",
+    '네': "네냥",
+    '야': "야냥",
+    '꺼': "꺼냥",
+    '래': "래냥",
+    '해': "해냥",
+    '지': "지냥",
+    '라': "라냥",
+    '요': "요냥",
+    '가': "가냥",
+    '데': "데냥",
+    '돼': "돼냥",
+    '줘': "줘냥",
+    '마': "마냥",
+    '와': "와냥",
+    '어': "어냥",
+    '자': "자냥",
+    '죠': "죠냥",
+    '서': "서냥",
+    '게': "게냥",
+    // '임': "임냥",
+};
 
-var punctuationMarks = [".", ",", "?", "!", ";", "~", "^", "@", "(", ")", " "];
+var nyaCharacters = [
+    '.',
+    ',',
+    '?',
+    '!',
+    ' ',
+];
 
 function replacePunctuation(input: string): string { // yeah I used gpt to write this I don't know RegEx
     return input.replace(/(^|\s)([?!,.;~^@()]+)/g, (match, p1, p2) => {
         const firstChar = p2[0];
         let transformed;
-        
-        if (firstChar === '?') transformed = chaePrefix + '?';
-        else if (firstChar === '!') transformed = chaePrefix + '!';
-        else if (firstChar === ',') transformed = chaePrefix + ',';
-        else if (firstChar === '.') transformed = chaePrefix + '.';
-        else if (firstChar === ';') transformed = chaePrefix + ';';
-        else if (firstChar === '~') transformed = chaePrefix + '~';
-        else if (firstChar === '^') transformed = chaePrefix + '^';
-        else if (firstChar === '@') transformed = chaePrefix + '@';
-        else if (firstChar === '(') transformed = chaePrefix + '(';
-        else if (firstChar === ')') transformed = chaePrefix + ')';
+
+        if (firstChar === '?') transformed = '냥?';
+        else if (firstChar === '!') transformed = '냥!';
+        else if (firstChar === ',') transformed = '냥,';
+        else if (firstChar === '.') transformed = '냥.';
+        else if (firstChar === ';') transformed = '냥;';
+        else if (firstChar === '~') transformed = '냥~';
+        else if (firstChar === '^') transformed = '냥^';
+        else if (firstChar === '@') transformed = '냥@';
+        else if (firstChar === '(') transformed = '냥(';
+        else if (firstChar === ')') transformed = '냥)';
         else transformed = p2;
-        
+
         return p1 + transformed + p2.slice(1);
     });
 }
@@ -111,7 +110,7 @@ function addNyangAtMWord(sentence: string): string { // Also from GPT
             const jongseong = baseCode % 28;
 
             if (jongseong === 16) {
-                return baseWord + Settings.plugins.Nya.chaePrefixValue + punctuation;
+                return baseWord + "냥" + punctuation;
             }
         }
 
@@ -121,24 +120,30 @@ function addNyangAtMWord(sentence: string): string { // Also from GPT
 
 function Nyaize(originalMessage) {
     for (let key in nyaWords2) {
-        const prefixValue = nyaWords2[key]();
-
-        for (let mark of punctuationMarks) {
-            originalMessage = originalMessage.replaceAll(key + mark, prefixValue + mark);
-        }
+        originalMessage = originalMessage.replaceAll(key + ".", nyaWords2[key] + "."); // yeah I gotta optimize these
+        originalMessage = originalMessage.replaceAll(key + ",", nyaWords2[key] + ",");
+        originalMessage = originalMessage.replaceAll(key + "?", nyaWords2[key] + "?");
+        originalMessage = originalMessage.replaceAll(key + "!", nyaWords2[key] + "!");
+        originalMessage = originalMessage.replaceAll(key + ";", nyaWords2[key] + ";");
+        originalMessage = originalMessage.replaceAll(key + "~", nyaWords2[key] + "~");
+        originalMessage = originalMessage.replaceAll(key + "^", nyaWords2[key] + "^");
+        originalMessage = originalMessage.replaceAll(key + "@", nyaWords2[key] + "@");
+        originalMessage = originalMessage.replaceAll(key + "(", nyaWords2[key] + "(");
+        originalMessage = originalMessage.replaceAll(key + ")", nyaWords2[key] + ")");
+        originalMessage = originalMessage.replaceAll(key + " ", nyaWords2[key] + " ");
 
         if (originalMessage.endsWith(key)) {
-            originalMessage = originalMessage.slice(0, -1) + prefixValue;
+            originalMessage = originalMessage.slice(0, -1) + nyaWords2[key];
         }
     }
 
     for (let key in nyaWords) {
-        originalMessage = originalMessage.replaceAll(key, nyaWords[key])
+        originalMessage = originalMessage.replaceAll(key, nyaWords[key]);
     }
 
-    originalMessage = replacePunctuation(originalMessage)
+    originalMessage = replacePunctuation(originalMessage);
 
-    originalMessage = addNyangAtMWord(originalMessage)
+    originalMessage = addNyangAtMWord(originalMessage);
 
     return originalMessage;
 }
@@ -153,17 +158,8 @@ export default definePlugin({
         },
     ],
 
-    options: {
-        chaePrefixValue: {
-            type: OptionType.STRING,
-            description: "~체 Prefix",
-            default: "냥",
-            onChange: refreshPrefix
-        }
-    },
-
     start() {
-        this.preSend = addPreSendListener(async (_, message) => {
+        this.preSend = addMessagePreSendListener(async (_, message) => {
             if (!message.content) return;
 
             message.content = await Nyaize(message.content);
@@ -171,6 +167,6 @@ export default definePlugin({
     },
 
     stop() {
-        removePreSendListener(this.preSend);
+        removeMessagePreSendListener(this.preSend);
     },
 });
